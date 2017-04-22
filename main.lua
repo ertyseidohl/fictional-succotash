c122b3b8f794fc794eb3c6555693c8dd2bd03029
Erty Seidohl <erty@codecraftschool.com>
2017-04-22 10:24:46 -0600

    Quit on escape

--- main.lua
+++ main.lua
@@ -1,3 +1,13 @@
+local WIDTH = 800
+local HEIGHT = 600
+
 function love.draw()
-    love.graphics.print('Hello World!', 400, 300)
+    love.graphics.print('Hello World!', WIDTH / 2, HEIGHT / 2)
 end
+
+function love.update()
+	if love.keyboard.isDown('escape') then
+		love.event.quit()
+	end
+end
+

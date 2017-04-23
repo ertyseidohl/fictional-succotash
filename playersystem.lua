8aaa3982e9a24155c62d131daced07cc9580e89a
Erty Seidohl <erty@codecraftschool.com>
2017-04-23 14:20:05 -0600

    10s continue per player after end of game

---
+++ playersystem.lua
@@ -0,0 +1,100 @@
+local PlayerSystem = class('PlayerSystem')
+
+function PlayerSystem:initialize()
+	self.credits = 0
+
+	self.playerStates = {
+		PLAYER_STATE_NONE,
+		PLAYER_STATE_NONE,
+		PLAYER_STATE_NONE,
+		PLAYER_STATE_NONE
+	}
+
+	self.playerCountdowns = {
+		0,
+		0,
+		0,
+		0
+	}
+end
+
+function PlayerSystem:addPlayer(player)
+	if self.playerStates[player] == PLAYER_STATE_NONE and
+		self:removeCredit()
+	then
+		self:setPlayerState(player, PLAYER_STATE_ALIVE)
+	end
+end
+
+function PlayerSystem:draw()
+	for i = 1, 4, 1 do
+		love.graphics.setColor(unpack(TEXT_COLOR))
+		if self.playerStates[i] == PLAYER_STATE_CONTINUE then
+			love.graphics.print("CONTINUE?", 100 * i, HEIGHT - 100)
+			love.graphics.print(self.playerCountdowns[i], 100 * i, HEIGHT - 75)
+		end
+	end
+end
+
+function PlayerSystem:update(dt, clock)
+	if clock.is_on_full then
+		for i = 1, 4, 1 do
+			if self.playerCountdowns[i] > 0 then
+				self.playerCountdowns[i] = self.playerCountdowns[i] - 1
+			elseif self.playerCountdowns[i] == 0 and self.playerStates[i] == PLAYER_STATE_CONTINUE then
+				self.playerStates[i] = PLAYER_STATE_NONE
+			end
+		end
+
+		if not self:hasPlayers() and gameState == STATE_GAME_OVER then
+			backToMenu()
+		end
+	end
+end
+
+function PlayerSystem:addCredit()
+	self.credits = self.credits + 1
+end
+
+function PlayerSystem:notifyOfDeath(player)
+	self:setPlayerState(player, PLAYER_STATE_CONTINUE)
+	self:startCountdown(player)
+end
+
+function PlayerSystem:startCountdown(player)
+	self.playerCountdowns[player] = GAME_OVER_COUNTDOWN_MAX
+end
+
+function PlayerSystem:setPlayerState(player, state)
+	self.playerStates[player] = state
+end
+
+function PlayerSystem:removeCredit()
+	if self.credits > 0 then
+		self.credits = self.credits - 1
+		return true
+	else
+		return false
+	end
+end
+
+function PlayerSystem:hasPlayers()
+	for i = 1, 4, 1 do
+		if self.playerStates[i] ~= PLAYER_STATE_NONE then
+			return true
+		end
+	end
+	return false
+end
+
+function PlayerSystem:getAlivePlayerCount()
+	local count = 0
+	for i = 1, 4, 1 do
+		if self.playerStates[i] == PLAYER_STATE_ALIVE then
+			count = count + 1
+		end
+	end
+	return count
+end
+
+return PlayerSystem
